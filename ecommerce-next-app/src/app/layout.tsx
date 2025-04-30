aa3a7504a8fd2d08b8f79f1b6e85bbc5cd8dfcb5
Sobaan Rana <sobaanrana@gmail.com>
2025-04-30 20:34:49 +0100

    Desktop Navbar added

--- ecommerce-next-app/src/app/layout.tsx
+++ ecommerce-next-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         )}
       >
         <main className="relative flex flex-col min-h-screen">
+          <Navbar />
           <div className="flex-grow flex-1"> {children}</div>
         </main>
       </body>
---
+++ ecommerce-next-app/src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "Clothing",
+    value: "clothing" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imageSrc: "/nav/clothing/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/clothing/mixed.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imageSrc: "/nav/clothing/mixed.jpg",
+      },
+    ],
+  },
+  {
+    label: "Electronics",
+    value: "electronics" as const,
+    featured: [
+      {
+        name: "Favourite Picks",
+        href: "#",
+        imageSrc: "/nav/clothing/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/clothing/mixed.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imageSrc: "/nav/clothing/mixed.jpg",
+      },
+    ],
+  },
+];
