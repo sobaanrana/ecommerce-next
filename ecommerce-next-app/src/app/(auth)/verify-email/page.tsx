dd40a46000f41ca62861ecca3a5177a819e89c9d
Sobaan Rana <sobaanrana@gmail.com>
2025-07-03 20:40:53 +0500

    verifyEmail page interface fixed

--- ecommerce-next-app/src/app/(auth)/verify-email/page.tsx
+++ ecommerce-next-app/src/app/(auth)/verify-email/page.tsx
@@ -1,18 +1,16 @@
 import VerifyEmail from "@/components/VerifyEmail";
 import Image from "next/image";
 
-interface PageProps {
-  searchParams?: {
-    [key: string]: string | string[];
-  };
-}
-const VerifyEmailPage = ({ searchParams }: PageProps) => {
-  const token = searchParams.token;
-  const toEmail = searchParams.to;
+const VerifyEmailPage = ({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) => {
+  const token = searchParams?.token;
+  const toEmail = searchParams?.to;
 
-  console.log(token);
   return (
-    <div className="container relative flex justify-center items-center  pt-20 lg:px-0 ">
+    <div className="container relative flex justify-center items-center pt-20 lg:px-0 ">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 ">
         {token && typeof token === "string" ? (
           <div className="grid gap-6">
@@ -23,9 +21,7 @@ const VerifyEmailPage = ({ searchParams }: PageProps) => {
             <div className="relative mb-4 h-60 w-60 text-muted-foreground">
               <Image src="/sent-email.png" fill alt="email sent image" />
             </div>
-
             <h3 className="font-semibold text-2xl">Check your email</h3>
-
             {toEmail ? (
               <p className="text-muted-foreground text-center">
                 {" "}
