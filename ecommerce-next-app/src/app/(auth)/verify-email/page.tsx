2fc3ba30c3deec8d3580fd6cf42cd55fb3c9e606
Sobaan Rana <sobaanrana@gmail.com>
2025-07-03 20:26:49 +0500

    verifyEmail comp fixed

--- ecommerce-next-app/src/app/(auth)/verify-email/page.tsx
+++ ecommerce-next-app/src/app/(auth)/verify-email/page.tsx
@@ -2,7 +2,7 @@ import VerifyEmail from "@/components/VerifyEmail";
 import Image from "next/image";
 
 interface PageProps {
-  searchParams: {
+  searchParams?: {
     [key: string]: string | string[] | undefined;
   };
 }
