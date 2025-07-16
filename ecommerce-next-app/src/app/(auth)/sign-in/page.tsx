382099ea13b9c0c41c6793b2f3cd5f3274d9be08
Sobaan Rana <sobaanrana@gmail.com>
2025-07-16 09:07:23 +0500

    vercel log error fixed

--- ecommerce-next-app/src/app/(auth)/sign-in/page.tsx
+++ ecommerce-next-app/src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Icons } from "@/components/Icons";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
